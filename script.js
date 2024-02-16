1098783ea869037581565cd035f24588b4b802a6
Mohammed Faiyaz <124358070+FaiyazMohammed000@users.noreply.github.com>
2024-02-16 19:54:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -89,5 +89,17 @@ function closeMenu() {
 menuItems.forEach(item => {
   item.addEventListener('click', closeMenu);
 });
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollTopButton").style.display = "block";
+  } else {
+    document.getElementById("scrollTopButton").style.display = "none";
+  }
+}
   
